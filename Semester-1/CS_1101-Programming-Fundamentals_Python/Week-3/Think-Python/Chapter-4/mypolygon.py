26d9c51b06866d0ec4cfde05d767527b8ec3a266
IanoNjuguna <inc3099@gmail.com>
2023-10-01 23:06:18 +0300

    mypolygon.py: draw a square using turtle

---
+++ Semester-1/CS_1101-Programming-Fundamentals_Python/Week-3/Think-Python/Chapter-4/mypolygon.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+import turtle
+
+bob = turtle.Turtle()
+
+print(bob)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+
+
+turtle.mainloop()
+
